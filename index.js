229ded693fbb940f1425fae76b192425800f56ac
ZuriPL <michal.bak222@gmail.com>
2022-11-24 20:00:12 +0100

    1

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// import first from './scraper.js'
-// import second from './mailer.js'
-// const first = require('./scraper.js')
-// const second = require('./mailer.js')
+import first from './scraper.js'
+import second from './mailer.js'
 import express from 'express'
 import 'dotenv/config'
-import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb'
-import fetch from 'node-fetch'
+// import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb'
+// import fetch from 'node-fetch'
 import nodemailer from 'nodemailer'
 
 const app = express()
@@ -70,109 +68,109 @@ async function mailer(reciever, skins) {
 	})
 }
 
-async function second() {
-	const promise = new Promise((res, rej) => {
-		const uri = process.env.URI
-		const client = new MongoClient(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			serverApi: ServerApiVersion.v1,
-		})
-		client.connect(async (err) => {
-			const collection = client.db('SkinsTracker').collection('SkinsTracker')
-			let data = await collection.findOne({ _id: ObjectId('62923f1b36006ed1642ff7d5') })
+// async function second() {
+// 	const promise = new Promise((res, rej) => {
+// 		const uri = process.env.URI
+// 		const client = new MongoClient(uri, {
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 			serverApi: ServerApiVersion.v1,
+// 		})
+// 		client.connect(async (err) => {
+// 			const collection = client.db('SkinsTracker').collection('SkinsTracker')
+// 			let data = await collection.findOne({ _id: ObjectId('62923f1b36006ed1642ff7d5') })
 
-			data.users.forEach((user) => {
-				let allSkins = []
-				user.skins.forEach((id) => allSkins.push(data.skins[id]))
-				try {
-					mailer(user.email, allSkins).catch(console.error)
-				} catch (e) {
-					pass
-				}
-			})
+// 			data.users.forEach((user) => {
+// 				let allSkins = []
+// 				user.skins.forEach((id) => allSkins.push(data.skins[id]))
+// 				try {
+// 					mailer(user.email, allSkins).catch(console.error)
+// 				} catch (e) {
+// 					pass
+// 				}
+// 			})
 
-			client.close()
-			res()
-		})
-	})
-	return await promise
-}
+// 			client.close()
+// 			res()
+// 		})
+// 	})
+// 	return await promise
+// }
 
-async function first() {
-	const promise = new Promise((res, rej) => {
-		const uri = process.env.URI
+// async function first() {
+// 	const promise = new Promise((res, rej) => {
+// 		const uri = process.env.URI
 
-		const client = new MongoClient(uri, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			serverApi: ServerApiVersion.v1,
-		})
+// 		const client = new MongoClient(uri, {
+// 			useNewUrlParser: true,
+// 			useUnifiedTopology: true,
+// 			serverApi: ServerApiVersion.v1,
+// 		})
 
-		client.connect(async (err) => {
-			async function scrape(id) {
-				let res = await fetch(`https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${id}`, {
-					headers: {
-						cookie: 'Locale-Supported=en',
-					},
-					credentials: 'include',
-					mode: 'cors',
-				})
-				let data = await res.json()
+// 		client.connect(async (err) => {
+// 			async function scrape(id) {
+// 				let res = await fetch(`https://buff.163.com/api/market/goods/sell_order?game=csgo&goods_id=${id}`, {
+// 					headers: {
+// 						cookie: 'Locale-Supported=en',
+// 					},
+// 					credentials: 'include',
+// 					mode: 'cors',
+// 				})
+// 				let data = await res.json()
 
-				let name = data.data.goods_infos[id].name
-				let price = data.data.items[0].price + ' ¥'
+// 				let name = data.data.goods_infos[id].name
+// 				let price = data.data.items[0].price + ' ¥'
 
-				return { name, price, id }
-			}
+// 				return { name, price, id }
+// 			}
 
-			const collection = client.db('SkinsTracker').collection('SkinsTracker')
-			let data = await collection.findOne({ _id: ObjectId('62923f1b36006ed1642ff7d5') })
+// 			const collection = client.db('SkinsTracker').collection('SkinsTracker')
+// 			let data = await collection.findOne({ _id: ObjectId('62923f1b36006ed1642ff7d5') })
 
-			let ids = []
-			data.users.forEach((user) => {
-				user.skins.forEach((skin) => {
-					ids.push(skin)
-				})
-			})
+// 			let ids = []
+// 			data.users.forEach((user) => {
+// 				user.skins.forEach((skin) => {
+// 					ids.push(skin)
+// 				})
+// 			})
 
-			ids = [...new Set(ids)]
+// 			ids = [...new Set(ids)]
 
-			client.close()
+// 			client.close()
 
-			let result = []
+// 			let result = []
 
-			for (const id of ids) {
-				result.push(await scrape(id))
-			}
+// 			for (const id of ids) {
+// 				result.push(await scrape(id))
+// 			}
 
-			let obj = {}
+// 			let obj = {}
 
-			result.forEach((result) => {
-				obj[result.id] = { name: result.name, price: result.price }
-			})
+// 			result.forEach((result) => {
+// 				obj[result.id] = { name: result.name, price: result.price }
+// 			})
 
-			console.log(obj)
+// 			console.log(obj)
 
-			client.connect(async (err) => {
-				const collection = client.db('SkinsTracker').collection('SkinsTracker')
+// 			client.connect(async (err) => {
+// 				const collection = client.db('SkinsTracker').collection('SkinsTracker')
 
-				await collection.updateOne(
-					{ _id: ObjectId('62923f1b36006ed1642ff7d5') },
-					{
-						$set: {
-							skins: obj,
-						},
-					}
-				)
+// 				await collection.updateOne(
+// 					{ _id: ObjectId('62923f1b36006ed1642ff7d5') },
+// 					{
+// 						$set: {
+// 							skins: obj,
+// 						},
+// 					}
+// 				)
 
-				client.close()
-				res()
-			})
-		})
-	})
-	return await promise
-}
+// 				client.close()
+// 				res()
+// 			})
+// 		})
+// 	})
+// 	return await promise
+// }
 
 async function taskWrapper(func, taskName) {
 	console.log(`[INFO] Started task ${taskName} at ${new Date()}`)
